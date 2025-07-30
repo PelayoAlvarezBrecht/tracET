e351095195aa4c796835cb176d1c46ef26483e3c
Fran-AM <francisco.aguilarm@um.es>
2025-07-30 16:56:41 +0200

    fix: Installation process

--- tracET/setup.py
+++ tracET/setup.py
@@ -1,28 +1,34 @@
 from setuptools import setup, Extension, find_packages
 import os
+import numpy
 
+# Define the C extension
+module1 = Extension(
+    'supression',
+    include_dirs=[
+        numpy.get_include(),
+        os.path.join(os.getcwd(), 'include'),
+    ],
+    sources=['cmodules/supressionmodule.c'],
+)
 
-module1 = Extension('supression',
-                    include_dirs=[os.path.join(os.getcwd(), 'include')],
-                   # libraries=['math','pthread','unistd','string'],
-                    #library_dirs=['/usr/local/lib'],
-                    sources = ['tracET/cmodules/supressionmodule.c',],)
-
-
-setup (name = 'tracET',
-       version = '2.2.2',
-       description = 'A package that could transform a scalar map in a point cloud, trace a graph for filaments and compute the dice metric for two segmentations.',
-       packages = find_packages(),
-       package_dir = {'tracET': './tracET'},
-       ext_modules = [module1],
-       entry_points = {
-           'console_scripts': [
-                    'get_saliency = tracET.scripts.get_saliency:main',
-                    'apply_nonmaxsup = tracET.scripts.apply_nonmaxsup:main',
-                    'trace_graph = tracET.scripts.trace_graph:main',
-                    'get_cluster = tracET.scripts.Get_cluster:main',
-                    'membrane_poly = tracET.scripts.membrane_poly:main',
-                    'seg_skel_dice = tracET.scripts.seg_skel_dice:main',
-           ],
-       },
+# Main setup call
+setup(
+    name='tracET',
+    version='2.2.2',
+    description='A package that could transform a scalar map in a point cloud, trace a graph for filaments and compute the dice metric for two segmentations.',
+    author='Pelayo Alvarez-Brecht, Antonio Martinez-Sanchez',
+    packages=find_packages(),
+    package_dir={'tracET': './tracET'},
+    ext_modules=[module1],
+    entry_points={
+        'console_scripts': [
+            'get_saliency = tracET.scripts.get_saliency:main',
+            'apply_nonmaxsup = tracET.scripts.apply_nonmaxsup:main',
+            'trace_graph = tracET.scripts.trace_graph:main',
+            'get_cluster = tracET.scripts.Get_cluster:main',
+            'membrane_poly = tracET.scripts.membrane_poly:main',
+            'seg_skel_dice = tracET.scripts.seg_skel_dice:main',
+        ],
+    },
 )
